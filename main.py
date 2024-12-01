7eaa889bfaf483b6304203ea3c06b7a74db4e390
TheWhiteDog9487 <gaojian520one@outlook.com>
2024-12-01 08:00:44 +0800

    1：保存的文件名改成了带有程序运行时间的格式，因此不再需要检查是否有同名文件。 2：获取数据过程中加入请求延迟，测试中，按需修改数值。 3：文件内容修正了card和extend_json没被正确序列化，导致文件内容格式异常的问题，另外非ASCII字符的保存正常了。 4：修正README文件名
    另：该API风控机制不明确，需要另外测试

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import asyncio
 import json
+from datetime import datetime
 from typing import Dict
 
 import aiohttp
-import asyncio
-import os
 
 UID = '401746666'
 URL = f'https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history?host_uid={UID}&need_top=1'
@@ -11,12 +11,17 @@ CookieFilePath = ""
 CookieFileName = "cookies.json"
 Headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0",
-    "Cookie": ""}
+    "Cookie": "",
+    "Referer":f"https://space.bilibili.com/{UID}/dynamic"}
 Continue = True
 Offset = 0
 Count = -1
 SaveFilePath = ""
-SaveFileName = "save.json"
+SaveFileName = "save"
+ExtensionFilename = ".json"
+TimeNow = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+SaveFileFullName = f"{SaveFilePath}{SaveFileName} - {TimeNow}{ExtensionFilename}"
+RequestRate = 1
 
 
 def Debug(Function):
@@ -40,23 +45,18 @@ def LoadCookie():
 
 def SaveToFile(Data: Dict):
     global SaveFilePath
-    with open(f"{SaveFilePath}{SaveFileName}", "a", encoding="utf-16") as f:
-        Json = json.dumps(Data, indent=4, ensure_ascii=True).encode().decode("unicode_escape").encode('utf-8', 'replace').decode('utf-8')
+    with open(SaveFileFullName, "a", encoding="utf-16") as f:
+        # Json = json.dumps(Data, indent=4, ensure_ascii=True).encode().decode("unicode_escape").encode('utf-8', 'replace').decode('utf-8')
+        Json = json.dumps(Data, indent=4, ensure_ascii=False)
         try:
             f.write(Json)
         except UnicodeEncodeError as e:
             pass
 
 
-def PrepareSaveFile():
-    if os.path.exists(f"{SaveFilePath}{SaveFileName}"):
-        os.remove(f"{SaveFilePath}{SaveFileName}")
-
-
 # @Debug
 async def main():
     LoadCookie()
-    PrepareSaveFile()
     global Offset
     global Continue
     global Count
@@ -64,8 +64,16 @@ async def main():
         if Count == 0:
             break
         async with aiohttp.ClientSession() as session:
+            await asyncio.sleep(RequestRate)
             async with session.get(URL + f"&offset_dynamic_id={Offset}", headers=Headers) as response:
+                if response.status == 412:
+                    raise RuntimeError(f"触发风控\n{await response.text()}")
                 Data = await response.json()
+                if Data["data"]["has_more"] == 0 and Data["data"]["cards"] is None:
+                    raise RuntimeError("请传递Cookie")
+                for c in Data["data"]["cards"]:
+                    c["card"] = json.loads(c["card"])
+                    c["extend_json"] = json.loads(c["extend_json"])
                 if Data["data"]["has_more"] == 1:
                     Offset = Data["data"]["next_offset"]
                     if Count > 0:
